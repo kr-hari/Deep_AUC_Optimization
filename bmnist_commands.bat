8f33e901c237adeb9f31b94bd74f578b275f4316
unknown <harikrishnankr16@gmail.com>
2023-05-29 10:20:54 -0500

    Final commmit - post submission

---
+++ bmnist_commands.bat
@@ -0,0 +1,58 @@
+python src/bmnist_AUCM_v1.py --download   --data_flag=breastmnist --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_dataset=double_val
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=pneumoniamnist --lr=1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=50 --based_on=val 
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=nodulemnist3d --lr=1 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=vesselmnist3d --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_dataset=halve_val 
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=adrenalmnist3d --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --modify_dataset=double_val
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=synapsemnist3d --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --modify_dataset=halve_val
+
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=breastmnist --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_dataset=double_val
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=pneumoniamnist --lr=1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=50 --based_on=val 
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=nodulemnist3d --lr=1 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=vesselmnist3d --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_dataset=halve_val 
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=adrenalmnist3d --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --modify_dataset=double_val
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=synapsemnist3d --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --modify_dataset=halve_val
+
+
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=breastmnist --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_dataset=double_val
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=pneumoniamnist --lr=1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=50 --based_on=val 
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=nodulemnist3d --lr=1 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=vesselmnist3d --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_dataset=halve_val 
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=adrenalmnist3d --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --modify_dataset=double_val
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=synapsemnist3d --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --modify_dataset=halve_val
+
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=breastmnist --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-1 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_dataset=double_val
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=pneumoniamnist --lr=1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-1 --epoch_decay=2e-5 --num_epochs=50 --based_on=val 
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=nodulemnist3d --lr=1 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-1 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=vesselmnist3d --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-1 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_dataset=halve_val 
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=adrenalmnist3d --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-1 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --modify_dataset=double_val
+
+python src/bmnist_AUCM_v1.py --download   --data_flag=synapsemnist3d --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-1 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --modify_dataset=halve_val
+
+
+
+
+
+
+
---
+++ bmnist_commands_v1.bat
@@ -0,0 +1,114 @@
+python src/bmnist_AUCM.py --data_flag=breastmnist --lr=1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=breastmnist --lr=1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=breastmnist --lr=1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=breastmnist --lr=1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=breastmnist --lr=1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_datasets=double_val
+
+python src/bmnist_AUCM.py --data_flag=breastmnist --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=breastmnist --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=breastmnist --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=breastmnist --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=breastmnist --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_datasets=double_val
+
+python src/bmnist_AUCM.py --data_flag=breastmnist --lr=0.01 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=breastmnist --lr=0.01 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=breastmnist --lr=0.01 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=breastmnist --lr=0.01 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=breastmnist --lr=0.01 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val --modify_datasets=double_val
+
+
+python src/bmnist_AUCM.py --data_flag=pneumoniamnist --lr=1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug
+python src/bmnist_AUCM.py --data_flag=pneumoniamnist --lr=1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug
+python src/bmnist_AUCM.py --data_flag=pneumoniamnist --lr=1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug
+python src/bmnist_AUCM.py --data_flag=pneumoniamnist --lr=1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug
+python src/bmnist_AUCM.py --data_flag=pneumoniamnist --lr=1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug
+
+python src/bmnist_AUCM.py --data_flag=pneumoniamnist --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug
+python src/bmnist_AUCM.py --data_flag=pneumoniamnist --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug
+python src/bmnist_AUCM.py --data_flag=pneumoniamnist --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug
+python src/bmnist_AUCM.py --data_flag=pneumoniamnist --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug
+python src/bmnist_AUCM.py --data_flag=pneumoniamnist --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug
+
+python src/bmnist_AUCM.py --data_flag=pneumoniamnist --lr=0.01 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug
+python src/bmnist_AUCM.py --data_flag=pneumoniamnist --lr=0.01 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug
+python src/bmnist_AUCM.py --data_flag=pneumoniamnist --lr=0.01 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug
+python src/bmnist_AUCM.py --data_flag=pneumoniamnist --lr=0.01 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug
+python src/bmnist_AUCM.py --data_flag=pneumoniamnist --lr=0.01 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug
+
+
+python src/bmnist_AUCM.py --data_flag=vesselmnist3d --lr=1 --batch_size= 16 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=vesselmnist3d --lr=1 --batch_size= 16 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=vesselmnist3d --lr=1 --batch_size= 16 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=vesselmnist3d --lr=1 --batch_size= 16 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=vesselmnist3d --lr=1 --batch_size= 16 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=halve_val
+
+python src/bmnist_AUCM.py --data_flag=vesselmnist3d --lr=0.1 --batch_size= 16 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=vesselmnist3d --lr=0.1 --batch_size= 16 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=vesselmnist3d --lr=0.1 --batch_size= 16 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=vesselmnist3d --lr=0.1 --batch_size= 16 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=vesselmnist3d --lr=0.1 --batch_size= 16 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=halve_val
+
+python src/bmnist_AUCM.py --data_flag=vesselmnist3d --lr=0.01 --batch_size= 16 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=vesselmnist3d --lr=0.01 --batch_size= 16 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=vesselmnist3d --lr=0.01 --batch_size= 16 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=vesselmnist3d --lr=0.01 --batch_size= 16 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=vesselmnist3d --lr=0.01 --batch_size= 16 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=halve_val
+
+
+python src/bmnist_AUCM.py --data_flag=adrenalmnist3d --lr=1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=adrenalmnist3d --lr=1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=adrenalmnist3d --lr=1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=adrenalmnist3d --lr=1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=adrenalmnist3d --lr=1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=double_val
+
+python src/bmnist_AUCM.py --data_flag=adrenalmnist3d --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=adrenalmnist3d --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=adrenalmnist3d --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=adrenalmnist3d --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=adrenalmnist3d --lr=0.1 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=double_val
+
+python src/bmnist_AUCM.py --data_flag=adrenalmnist3d --lr=0.01 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=adrenalmnist3d --lr=0.01 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=adrenalmnist3d --lr=0.01 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=adrenalmnist3d --lr=0.01 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=double_val
+python src/bmnist_AUCM.py --data_flag=adrenalmnist3d --lr=0.01 --batch_size=32 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val  --data_aug --modify_datasets=double_val
+
+
+
+python src/bmnist_AUCM.py --data_flag=synapsemnist3d --lr=1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=synapsemnist3d --lr=1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=synapsemnist3d --lr=1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=synapsemnist3d --lr=1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=synapsemnist3d --lr=1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_datasets=halve_val
+
+python src/bmnist_AUCM.py --data_flag=synapsemnist3d --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=synapsemnist3d --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=synapsemnist3d --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=synapsemnist3d --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=synapsemnist3d --lr=0.1 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_datasets=halve_val
+
+python src/bmnist_AUCM.py --data_flag=synapsemnist3d --lr=0.01 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=synapsemnist3d --lr=0.01 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=synapsemnist3d --lr=0.01 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=synapsemnist3d --lr=0.01 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_datasets=halve_val
+python src/bmnist_AUCM.py --data_flag=synapsemnist3d --lr=0.01 --batch_size=16 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val   --modify_datasets=halve_val
+
+python src/bmnist_AUCM.py --data_flag=nodulemnist3d --lr=1 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val 
+python src/bmnist_AUCM.py --data_flag=nodulemnist3d --lr=1 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val 
+python src/bmnist_AUCM.py --data_flag=nodulemnist3d --lr=1 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val 
+python src/bmnist_AUCM.py --data_flag=nodulemnist3d --lr=1 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val 
+python src/bmnist_AUCM.py --data_flag=nodulemnist3d --lr=1 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val 
+
+python src/bmnist_AUCM.py --data_flag=nodulemnist3d --lr=0.1 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val 
+python src/bmnist_AUCM.py --data_flag=nodulemnist3d --lr=0.1 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val 
+python src/bmnist_AUCM.py --data_flag=nodulemnist3d --lr=0.1 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val 
+python src/bmnist_AUCM.py --data_flag=nodulemnist3d --lr=0.1 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val 
+python src/bmnist_AUCM.py --data_flag=nodulemnist3d --lr=0.1 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val 
+
+python src/bmnist_AUCM.py --data_flag=nodulemnist3d --lr=0.01 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-3 --epoch_decay=2e-5 --num_epochs=80 --based_on=val 
+python src/bmnist_AUCM.py --data_flag=nodulemnist3d --lr=0.01 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-7 --epoch_decay=2e-5 --num_epochs=80 --based_on=val 
+python src/bmnist_AUCM.py --data_flag=nodulemnist3d --lr=0.01 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-2 --epoch_decay=2e-5 --num_epochs=80 --based_on=val 
+python src/bmnist_AUCM.py --data_flag=nodulemnist3d --lr=0.01 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-5 --epoch_decay=2e-5 --num_epochs=80 --based_on=val 
+python src/bmnist_AUCM.py --data_flag=nodulemnist3d --lr=0.01 --batch_size=64 --optimizer=PESG --loss=AUCM --weight_decay=1e-4 --epoch_decay=2e-5 --num_epochs=80 --based_on=val 
+
+
